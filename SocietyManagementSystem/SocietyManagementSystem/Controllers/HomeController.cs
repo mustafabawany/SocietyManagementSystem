a762c293027a2c0b6212eea98162662ea18a5be1
Syed Ahsan <syedahsan13@hotmail.com>
2022-11-26 20:02:08 +0500

    Added Navigation

--- SocietyManagementSystem/SocietyManagementSystem/Controllers/HomeController.cs
+++ SocietyManagementSystem/SocietyManagementSystem/Controllers/HomeController.cs
@@ -10,26 +10,10 @@ namespace SocietyManagementSystem.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult EventsList()
-        {
-            return View();
-        }
     }
 }
