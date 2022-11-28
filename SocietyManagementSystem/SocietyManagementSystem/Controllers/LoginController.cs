66df166fa79384d5261b54b8034c1acec140a1e8
Syed Ahsan <syedahsan13@hotmail.com>
2022-11-28 20:27:08 +0500

    Added Login Frontend

---
+++ SocietyManagementSystem/SocietyManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SocietyManagementSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        public ActionResult Login(FormCollection collection)
+        {
+            //Method 2: Using Component Index Position
+            //ViewData["StudentID"] = collection[1];
+            //ViewData["Name"] = collection[2];
+            //FormsAuthentication.SetAuthCookie("Ahsan",);
+            return View();
+        }
+
+    }
+}
